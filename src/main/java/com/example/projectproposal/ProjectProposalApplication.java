7c1d1da566021417c9517258bd8671255586c70f
kevin-sys <ksinasignargomez@unicesar.edu.co>
2023-11-23 10:41:56 -0500

    Add swagger2

--- src/main/java/com/example/projectproposal/ProjectProposalApplication.java
+++ src/main/java/com/example/projectproposal/ProjectProposalApplication.java
@@ -3,7 +3,10 @@ package com.example.projectproposal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ProjectProposalApplication {
 
     public static void main(String[] args) {
