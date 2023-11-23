07072b8cceb7608ed54184647bbe26f14d7bb492
kevin-sys <ksinasignargomez@unicesar.edu.co>
2023-11-23 10:47:15 -0500

    remove swagger

--- src/main/java/com/example/projectproposal/ProjectProposalApplication.java
+++ src/main/java/com/example/projectproposal/ProjectProposalApplication.java
@@ -3,10 +3,7 @@ package com.example.projectproposal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnableSwagger2
 public class ProjectProposalApplication {
 
     public static void main(String[] args) {
