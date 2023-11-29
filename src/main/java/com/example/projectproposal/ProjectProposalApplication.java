8a9a9cf32d81d8a743a842fcd79ffccbb4073640
kevin-sys <ksinasignargomez@unicesar.edu.co>
2023-11-28 23:09:22 -0500

    allow origin

--- src/main/java/com/example/projectproposal/ProjectProposalApplication.java
+++ src/main/java/com/example/projectproposal/ProjectProposalApplication.java
@@ -2,6 +2,9 @@ package com.example.projectproposal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjectProposalApplication {
@@ -10,4 +13,16 @@ public class ProjectProposalApplication {
         SpringApplication.run(ProjectProposalApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Mapea a todas las rutas
+                        .allowedOrigins("*") // Permite todos los orígenes
+                        .allowedMethods("*") // Permite todos los métodos (GET, POST, PUT, etc.)
+                        .allowedHeaders("*"); // Permite todas las cabeceras
+            }
+        };
+    }
 }
